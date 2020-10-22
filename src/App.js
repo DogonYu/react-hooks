37d54f32de4c152fc4c59fafb9315024d9b39d75
DogonYu <dorogon01@gmail.com>
2020-10-22 19:24:18 +0900

    add: useFullScreen

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useRef, useEffect } from 'react';
 const App = () => {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <button onClick={triggerFull}>Make fullscreen</button>
     </div>
   );
 };
