9423272b5a58055dd0dedf0d56eeb103be45ac1b
DogonYu <dorogon01@gmail.com>
2020-10-22 12:57:15 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useInput } from './useInput'
+import { useTabs } from './useTabs'
+import { useTitle } from './useTitle'
+import { useClick } from './useClick'
+
+const sections = [{
+  tab: "section 1", content: "I'm Section 1"
+},{
+  tab: "section 2", content: "I'm Section 2"
+}]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
     </div>
   );
 }
---
+++ src/useClick.js
@@ -0,0 +1,12 @@
+import { useEffect, useRef } from 'react'
+export const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) element.current.addEventListener("click", onClick);
+    return () => {
+      if (element.current) element.current.addEventListener("click", onClick);
+    };
+  }, [onClick]);
+  if (typeof onClick !== "function") return;
+  return element;
+};
