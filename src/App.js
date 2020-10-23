e635fc9082c66c2df4aac2510df3eed928ee551a
DogonYu <dorogon01@gmail.com>
2020-10-23 14:20:17 +0900

    add: useAxios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useRef, useEffect } from 'react';
+import useAxios from './useAxios';
 
 const App = () => {
+  const { loading, data, error, refetch } = useAxios({
+    url: 'https://cors-anywhere.herokuapp.com/https://yts.am/api/v2/list_movies.json',
+  });
   return (
     <div className="App">
-      <button>hello</button>
+      <div>{data && data.status}</div>
+      <div>{loading && 'Loading'}</div>
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
 };
